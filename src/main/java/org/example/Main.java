bc57308411548382712dd2506fb1b93125e06afa
Skander228 <kkhemiriskander@gmail.com>
2023-02-17 16:23:42 +0300

    telegram bot implemented 2 - 5

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
 
-    private static  final String BOT_TOKEN = "5808415408:AAGwAgxApnrZ1Yh-mwCqauFksyIfZqjKXo4";
+    private static final String BOT_TOKEN = "5808415408:AAGwAgxApnrZ1Yh-mwCqauFksyIfZqjKXo4";
     public static void main(String[] args) {
         TelegramBotApplication application = TelegramBotApplication.builder()
                 .botToken(BOT_TOKEN)
